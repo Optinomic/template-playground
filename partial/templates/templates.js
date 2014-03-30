7923315679e7e5addefc39dd4dc3897135d257bf
Beat Ottiger <ottigerb@gmail.com>
2014-03-30 16:41:40 +0200

    Again template-update

--- partial/templates/templates.js
+++ partial/templates/templates.js
@@ -1,5 +1,16 @@
 angular.module('optinomic').controller('TemplatesCtrl',function($scope, $routeParams){
 
+
+
+    // Get current directives info - the stupid way
+    for (var i = 0; i < $scope.directives.length; i++) {
+        if ($scope.directives[i].id === $routeParams.id) {
+            $scope.current_directive = $scope.directives[i];
+            //console.log ('current=', $scope.current_directive);
+        }
+    };
+
+
     $scope.current_directive_id = $routeParams.id;
     $scope.current_directive_include = 'partial/templates/sub/' + $routeParams.id + '.html';
 
--- service/directory_list.js
+++ service/directory_list.js
@@ -5,12 +5,21 @@ angular.module('optinomic').factory('directoryList',function() {
     var optinomic_directories = [
         {
             "title":"Title / Subtitle with Infoline",
+            "description":"A nice looking title for your cards.",
             "version":"1.0",
             "lastUpdate":"2014-03-30",
             "id" : "card-title"
         },
         {
+            "title":"Score with threshold",
+            "description":"A simple score based on threshold. Typically BDI-II.",
+            "version":"1.0",
+            "lastUpdate":"2014-03-30",
+            "id" : "score-threshold"
+        },
+        {
             "title":"Stanine",
+            "description":"Stanine (STAndard NINE) is a method of scaling test scores on a nine-point standard scale with a mean of five and a standard deviation of two.",
             "version":"1.0",
             "lastUpdate":"2014-03-30",
             "id" : "stanine"
