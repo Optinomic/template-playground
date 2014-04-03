bb0059f1bcff0b9599925554e709641b515f7dd9
Beat Ottiger <ottigerb@gmail.com>
2014-04-03 13:15:25 +0200

    Update <score-threshold>

--- directive/score-threshold/score-threshold.js
+++ directive/score-threshold/score-threshold.js
@@ -7,13 +7,26 @@ angular.module('optinomic').directive('scoreThreshold', function() {
             myscore: '=score',
             mymax: '=max',
             mymin: '=min',
-            myshow_min_max: '=showMinMax'
+            myshow_min_max: '=showMinMax',
+            myshow_percent_scale: '=showPercentScale',
+            myshow_scale_ranges: '=scaleRanges'
         },
         templateUrl: 'directive/score-threshold/score-threshold.html',
         link: function(scope, element, attrs, ngModel) {
 
+            function get_position(my_value) {
+                my_value = (100 / (scope.max - scope.min) * my_value) / 100;
+                my_value = my_value * 85 + 7;
+                return my_value;
+            }
+
 
             function just_do_it() {
+                //Variablen initialisieren
+                scope.score = scope.myscore;
+                scope.max = scope.mymax;
+                scope.min = scope.mymin;
+
 
                 if (scope.myshow_min_max === false) {
                     scope.show_min_max = false;
@@ -21,13 +34,40 @@ angular.module('optinomic').directive('scoreThreshold', function() {
                     scope.show_min_max = true;
                 }
 
+                if (scope.myshow_percent_scale === false) {
+                    scope.show_percent_scale = false;
+                } else {
+                    scope.show_percent_scale = true;
+                }
 
-                scope.score = scope.myscore;
-                scope.max = scope.mymax;
-                scope.min = scope.mymin;
+                if (scope.myshow_scale_ranges === undefined) {
+                    console.log('No Scale Ranges.');
+                    scope.show_scale_ranges = false;
+                } else {
+                    scope.show_scale_ranges = true;
 
-                scope.postition = (100 / (scope.max - scope.min) * scope.score) / 100;
-                scope.postition = scope.postition * 85 + 7;
+                    //Loop durch alle Scales
+                    for (var i = 0; i < scope.myshow_scale_ranges.ranges.length; i++) {
+                        scope.myshow_scale_ranges.ranges[i].from_position = get_position(scope.myshow_scale_ranges.ranges[i].from);
+                        scope.myshow_scale_ranges.ranges[i].to_position = get_position(scope.myshow_scale_ranges.ranges[i].to);
+                        scope.myshow_scale_ranges.ranges[i].width_position = scope.myshow_scale_ranges.ranges[i].to_position - scope.myshow_scale_ranges.ranges[i].from_position;
+
+                        if ( (scope.score >= scope.myshow_scale_ranges.ranges[i].from) && (scope.score <= scope.myshow_scale_ranges.ranges[i].to) ) {
+                            scope.myshow_scale_ranges.ranges[i].fill_color = "#EE3B16";
+
+                        } else {
+                            scope.myshow_scale_ranges.ranges[i].fill_color = "#C2C2C2";
+                        }
+
+                        console.log('LOOP = ', scope.myshow_scale_ranges.ranges[i]);
+
+
+                    }
+
+                    console.log('Scale Ranges = ', scope.myshow_scale_ranges);
+                }
+
+                scope.postition = get_position(scope.score);
 
 
                 if (scope.postition >= 50) {
--- partial/templates/templates.js
+++ partial/templates/templates.js
@@ -1,4 +1,4 @@
-angular.module('optinomic').controller('TemplatesCtrl',function($scope, $routeParams){
+angular.module('optinomic').controller('TemplatesCtrl', function($scope, $routeParams) {
 
     // Get current directives info - the stupid way
     for (var i = 0; i < $scope.directives.length; i++) {
@@ -12,8 +12,37 @@ angular.module('optinomic').controller('TemplatesCtrl',function($scope, $routePa
     $scope.current_directive_id = $routeParams.id;
     $scope.current_directive_include = 'partial/templates/sub/' + $routeParams.id + '.html';
 
+
+    //<card-title>
     $scope.templates_titel_titel_1 = 'Lebenszufriedenheit und Glück';
     $scope.templates_titel_titel_2 = 'Persönliche Verhaltensweisen und Gewohnheiten, 33 Aussagen.';
     $scope.templates_titel_titel_3 = 'Muster Max';
+
+    //<score>
+
+
+    $scope.scale_ranges = {
+        "ranges": [{
+            "from": 0,
+            "to": 8,
+            "result": "Keine Depression"
+        }, {
+            "from": 9,
+            "to": 13,
+            "result": "Minimale Depression"
+        }, {
+            "from": 14,
+            "to": 19,
+            "result": "Leichte Depression"
+        }, {
+            "from": 20,
+            "to": 28,
+            "result": "Mittelschwere Depression"
+        }, {
+            "from": 29,
+            "to": 63,
+            "result": "Schwere Depression"
+        }]
+    }
 
 });
