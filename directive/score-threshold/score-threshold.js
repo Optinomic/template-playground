a45a1d9cad05323370b55164d0a907c8acb4a136
Beat Ottiger <ottigerb@gmail.com>
2014-04-03 17:13:51 +0200

    <score-threshold> Update

--- directive/score-threshold/score-threshold.js
+++ directive/score-threshold/score-threshold.js
@@ -27,7 +27,6 @@ angular.module('optinomic').directive('scoreThreshold', function() {
                 scope.max = scope.mymax;
                 scope.min = scope.mymin;
 
-
                 if (scope.myshow_min_max === false) {
                     scope.show_min_max = false;
                 } else {
@@ -41,44 +40,34 @@ angular.module('optinomic').directive('scoreThreshold', function() {
                 }
 
                 if (scope.myshow_scale_ranges === undefined) {
-                    console.log('No Scale Ranges.');
                     scope.show_scale_ranges = false;
                 } else {
                     scope.show_scale_ranges = true;
 
                     //Loop durch alle Scales
                     for (var i = 0; i < scope.myshow_scale_ranges.ranges.length; i++) {
+                        //console.log('Loop - score = ', scope.myscore);
                         scope.myshow_scale_ranges.ranges[i].from_position = get_position(scope.myshow_scale_ranges.ranges[i].from);
                         scope.myshow_scale_ranges.ranges[i].to_position = get_position(scope.myshow_scale_ranges.ranges[i].to);
                         scope.myshow_scale_ranges.ranges[i].width_position = scope.myshow_scale_ranges.ranges[i].to_position - scope.myshow_scale_ranges.ranges[i].from_position;
 
-                        if ( (scope.score >= scope.myshow_scale_ranges.ranges[i].from) && (scope.score <= scope.myshow_scale_ranges.ranges[i].to) ) {
-                            scope.myshow_scale_ranges.ranges[i].fill_color = "#EE3B16";
-
-                        } else {
-                            scope.myshow_scale_ranges.ranges[i].fill_color = "#C2C2C2";
+                        if ((scope.myscore >= scope.myshow_scale_ranges.ranges[i].from) && (scope.myscore <= scope.myshow_scale_ranges.ranges[i].to)) {
+                            scope.my_current_scale = i;
                         }
-
-                        console.log('LOOP = ', scope.myshow_scale_ranges.ranges[i]);
-
-
+                        //console.log('LOOP = ', scope.myshow_scale_ranges.ranges[i]);
                     }
-
-                    console.log('Scale Ranges = ', scope.myshow_scale_ranges);
+                    //console.log('Scale Ranges = ', scope.myshow_scale_ranges);
                 }
 
                 scope.postition = get_position(scope.score);
 
-
                 if (scope.postition >= 50) {
-                    scope.postition_score = scope.postition - 2;
+                    scope.postition_score = scope.postition - 1.5;
                     scope.postition_align = 'end';
                 } else {
-                    scope.postition_score = scope.postition + 2;
+                    scope.postition_score = scope.postition + 1.5;
                     scope.postition_align = 'start';
                 }
-
-
             }
 
             just_do_it();
@@ -93,8 +82,6 @@ angular.module('optinomic').directive('scoreThreshold', function() {
             scope.$watch('mymin', function() {
                 just_do_it();
             }, true);
-
-            //console.log ('Titel', scope);
         }
     };
 });
